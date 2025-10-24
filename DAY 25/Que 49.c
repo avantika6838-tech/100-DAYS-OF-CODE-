f8694a45052a95be11a02e356e8361692cdf1c42
avantika6838-tech <avantika.6838@gmail.com>
2025-10-24 16:25:42 +0530

    Que 49.c
    Implemented a program to print a specific number pattern.

---
+++ DAY 25/Que 49.c
@@ -0,0 +1,26 @@
+/*DAY 25 OF C PROGRAMMING CODING
+Write a program to print the following pattern:
+5
+45
+345
+2345
+12345
+*/ 
+ #include <stdio.h>
+
+int main() {
+	printf("Name - Avantika\nSap ID - 590028347\nCourse - Bsc CS\nBatch - 1st");
+    	printf("\n-----------------------------------\n");
+
+
+    int i, j;
+    for (i = 5; i >= 1; i--) {
+       
+        for (j = i; j <= 5; j++) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
